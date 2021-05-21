7c99e8edb44d1c5c6c35583096d0db64b582e9bf
Odhiambo Atieno <odhiamboisatieno@gmail.com>
2021-05-21 12:50:48 -0400

    Update manage-brands.php

--- admin/manage-brands.php
+++ admin/manage-brands.php
@@ -83,7 +83,7 @@ $msg="Page data updated  successfully";
 				<div class="row">
 					<div class="col-md-12">
 
-						<h2 class="page-title">Manage Brands</h2>
+						<h2 class="page-title">Manage Vehicle Brands</h2>
 
 						<!-- Zero Configuration Table -->
 						<div class="panel panel-default">
