f882ada80c982d4fa728a9324a506d7954b7602a
Odhiambo Atieno <odhiamboisatieno@gmail.com>
2021-05-26 07:37:58 -0400

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -2,10 +2,10 @@
           <ul>
             <li><a href="profile.php">Profile Settings</a></li>
               <li><a href="update-password.php">Update Password</a></li>
-            <li><a href="my-booking.php">My Booking</a></li>
+            <li><a href="my-booking.php">My Bookings</a></li>
             <li><a href="post-testimonial.php">Post a Testimonial</a></li>
                <li><a href="my-testimonials.php">My Testimonials</a></li>
-            <li><a href="logout.php">Sign Out</a></li>
+            <li><a href="logout.php">Log Out</a></li>
           </ul>
         </div>
       </div>
